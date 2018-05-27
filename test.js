91e3e05a3a84941221532c1815d8c782c3a7deb7
MarieGorin <marie.gorin@essec.edu>
2018-05-27 18:31:13 +0200

    dynamic4

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-//setTimeout(function addElement(){
-document.body.onload = addElement;
 
-function addElement(){
-
+setTimeout(function addElement(){
     var newDiv = document.createElement("div");
     var newContent = document.createTextNode("If you are interested in my profile, do not hesitate to contact me!");
     newDiv.appendChild(newContent);
     var currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
-}
-//,10000);
+}, 5000);
+
+
 
 
 
