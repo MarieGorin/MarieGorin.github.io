b4be0194c91909dccd0bbab0c632f11b34990456
MarieGorin <marie.gorin@essec.edu>
2018-05-27 18:13:04 +0200

    Dynamic

---
+++ test.js
@@ -0,0 +1,10 @@
+setTimeout(function addElement(){
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("If you are interested in my profile, do not hesitate to contact me!");
+    newDiv.appendChild(newContent);
+    var currentDiv = document.getElementById("theEnd");
+    document.body.insertBefore(newDiv, currentDiv);
+},10000);
+
+
+
