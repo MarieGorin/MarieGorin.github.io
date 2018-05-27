ece95f105c9bcf404bc7267d2ddf41e3b0bd3f77
MarieGorin <marie.gorin@essec.edu>
2018-05-27 18:57:13 +0200

    child

--- test.js
+++ test.js
@@ -3,8 +3,7 @@ setTimeout(function addElement(){
     var newDiv = document.createElement("div");
     var newContent = document.createTextNode("If you are interested in my profile, do not hesitate to contact me!");
     newDiv.appendChild(newContent);
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
+    document.getElementById("div1").appendChild(newDiv);
     console.log('it worked!');
 }, 5000);
 
