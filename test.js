342813aa5f0726f597ef1d8edfa43e3aed3b2cd9
MarieGorin <marie.gorin@essec.edu>
2018-05-29 09:28:32 +0200

    Alert

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 
 setTimeout(function addElement(){
     var newDiv = document.createElement("div");
+    newDiv.classList.add("alert");
+    newDiv.classList.add("alert-secondary");
     var newContent = document.createTextNode("If you are interested in my profile, do not hesitate to contact me!");
     newDiv.appendChild(newContent);
     document.getElementById("div1").appendChild(newDiv);
